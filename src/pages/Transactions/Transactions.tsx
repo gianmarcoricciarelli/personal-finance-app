5ce90b6c9225172b6db15a3612d082246520c5b8
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2025-01-16 15:34:25 +0100

    feature(transactions): init transaction page

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,3 +1,18 @@
+import Text from '@components/Text/Text'
+import clsx from 'clsx'
+
 export default function Transactions() {
-    return <div>Hello</div>
+    return (
+        <div
+            className={clsx(
+                'px-4 tablet:px-10 desktop:px-10 py-6 tablet:py-8 desktop:py-8',
+                'flex flex-col gap-8',
+                'bg-pfa-beige-100'
+            )}
+        >
+            <Text fontSize='xl' fontStyle='bold' color='pfa-grey-900'>
+                Transactions
+            </Text>
+        </div>
+    )
 }
